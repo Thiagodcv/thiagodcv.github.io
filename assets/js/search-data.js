670c9277a0eaee7d5cc3a86f02bcae814397c426
Thiagodcv <64381250+Thiagodcv@users.noreply.github.com>
2025-09-08 21:53:19 +0000

    Deploying to gh-pages from @ Thiagodcv/thiagodcv.github.io@d2972bb09a3321c86dc6c3f0889997c9054b6990 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,6 +412,11 @@ ninja.data = [{
           description: "Implementing nonlinear model predictive control with box constraints using Julia",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
+            },},{id: "projects-model-based-reinforcement-learning-via-predictive-sampling",
+          title: 'Model-based Reinforcement Learning via Predictive Sampling',
+          description: "Learning a dynamics function for controlling a quadruped using PyTorch",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project.html";
             },},{
         id: 'social-email',
         title: 'email',
